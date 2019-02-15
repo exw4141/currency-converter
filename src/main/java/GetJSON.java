914af546e65afece713d02540a32100cfc18f84e
exw4141 <exw4141@rit.edu>
2019-02-15 00:26:06 -0500

    Added class to retrieve currency rate from exchange rate API

---
+++ src/main/java/GetJSON.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * GetJSON connects to an exchange rate API to obtain currency rate data.
+ * It is currently defaulted to retrieve rates relative to the U.S. dollar.
+ */
+public class GetJSON {
+
+    /**
+     * Sends a GET request to the exchange rate API to obtain currency rate data and store it in a String.
+     */
+    public static void getJSON() {
+        try {
+            URL url = new URL("https://api.exchangeratesapi.io/latest?base=USD");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputRead;
+            StringBuilder content = new StringBuilder();
+            while ((inputRead = in.readLine()) != null) {
+                content.append(inputRead);
+            }
+            in.close();
+
+            System.out.println(content);
+        } catch (MalformedURLException e) {
+            System.out.println("Invalid URL");
+        } catch (IOException e) {
+            System.out.println("Connection to API could not be established");
+        }
+    }
+}
